e2f4047e4ea26d4c0d20836ff359a7a1602c3fdf
felixanna <yuecnu@hotmail.com>
2023-12-20 22:46:42 +0800

    prepare publish to windows store

--- Services/Words/Models/AddWordHistoryModel.cs
+++ Services/Words/Models/AddWordHistoryModel.cs
@@ -5,7 +5,7 @@ namespace Services.Words.Models
     public class AddWordHistoryModel : AddWordModel
     {
         public int Id { get; set; }
-        public IList<string> CheckingHistories { get; set; }
-        public IList<bool> History => CheckingHistories!.Select(x => x.Trim() == "1" || string.Equals(x.Trim(), CheckingRemark.Correct.ToString(), StringComparison.OrdinalIgnoreCase)).ToArray();
+        public IList<string>? CheckingHistories { get; set; }
+        public IList<bool> History => CheckingHistories?.Select(x => x.Trim() == "1" || string.Equals(x.Trim(), CheckingRemark.Correct.ToString(), StringComparison.OrdinalIgnoreCase)).ToArray();
     }
 }
